6979a49c7cf0149c15c8fe048b29a6e944836ef1
Sélim <selim.boudaa@sinusoide.fr>
2018-10-12 12:19:53 +0200

    initial commit

---
+++ application/views/front/form.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title>BetaGouv - Signalement</title>
+    <meta charset="utf-8">    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+</head>
+
+<body id="top">
+<?php echo $title; ?>
+
+</body>
+</html>
