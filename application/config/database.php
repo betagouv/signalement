859454754eb2dcf562f01ef1119828cf62cc8a92
Sélim <selim.boudaa@sinusoide.fr>
2018-10-16 11:29:46 +0200

    Variable d'environnement pour l'host

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => getenv('DATABASE_URL'),
 	'username' => '',
 	'password' => '',
 	'database' => '',
