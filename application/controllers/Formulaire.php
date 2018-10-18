47f3211072a9df912bd14ca5666d38ace5973624
Sélim <selim.boudaa@sinusoide.fr>
2018-10-18 11:36:41 +0200

    mise en place mécanique de chgt des données des menus déroulants via fichiers CSV

--- application/controllers/Formulaire.php
+++ application/controllers/Formulaire.php
@@ -4,6 +4,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Formulaire extends CI_Controller {
 	public function index()
 	{
-		$this->load->view('formulaire');
+		$file = fopen(base_url()."assets/data/anomalies.csv", "r");
+		$anomalies_array = array();
+		while($csv_array=fgetcsv($file,1024,';'))
+		{
+			$anomalies_array[] = $csv_array;
+		}
+
+
+		$data['anomalies_json'] = json_encode($anomalies_array);
+		$this->load->view('formulaire',$data);
 	}
 }
