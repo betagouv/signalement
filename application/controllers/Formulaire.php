783536f57e2f1bb7bac227fe000e5afca7f6eb01
Sélim <selim.boudaa@sinusoide.fr>
2018-10-23 15:48:45 +0200

    - vérification de la saisie côté backend (réinjection data...) - génération de baliseshtml via les helpers codeigniter - agencement txt

--- application/controllers/Formulaire.php
+++ application/controllers/Formulaire.php
@@ -2,21 +2,71 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Formulaire extends CI_Controller {
+
 	public function index()
 	{
+		$config = array(
+			array(
+				'field' => 'place-category',
+				'label' => 'Type d\'établissement',
+				'rules' => 'required'
+			),
+			array(
+				'field' => 'type',
+				'label' => 'Type d\'anomalie',
+				'rules' => 'required'
+			),
+			array(
+				'field' => 'anomalies',
+				'label' => 'Anomalie',
+				'rules' => 'required'
+			),
+			array(
+				'field' => 'place',
+				'label' => 'Nom du commerçant',
+				'rules' => 'required'
+			),
+			array(
+				'field' => 'place-address',
+				'label' => 'Adresse de l\'établissement',
+				'rules' => 'required'
+			),
+			array(
+				'field' => 'name',
+				'label' => 'Prénom et nom',
+				'rules' => 'required'
+			),
+			array(
+				'field' => 'email',
+				'label' => 'E-mail',
+				'rules' => 'required|valid_email'
+			)
+		);
+
+		$this->form_validation->set_rules($config);
+
+		$this->form_validation->set_message('place-category', 'Error Message');
+
+		if ($this->form_validation->run())
+		{
+			$this->load->view('formulaire/submitted');
+		}
+		else
+		{
+			$data['anomalies_array'] = $this-> readCSV();
+			$this->load->view('formulaire',$data);
+		}
+	}
+
+	private function readCSV(){
 		$file = fopen("assets/data/anomalies.csv", "r");
 		$anomalies_array = array();
 		while($csv_array=fgetcsv($file,1024,','))
 		{
 			$anomalies_array[] = $csv_array;
 		}
-		$data['anomalies_json'] = json_encode($anomalies_array);
 
 
-		if (isset($_POST['anomalies'])) {
-			echo 'test';
-		}
-
-		$this->load->view('formulaire',$data);
+		return $anomalies_array;
 	}
 }
