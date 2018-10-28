e6b2e6e8945abd130a80b2cfb46e864be93050f9
Sélim <selim.boudaa@sinusoide.fr>
2018-10-28 15:59:22 +0100

    Modifications suite tests utilisateurs : esapcement, chgt d'intitulés ...

--- application/views/formulaire.php
+++ application/views/formulaire.php
@@ -1,20 +1,14 @@
 <div class="container mb-5">
     <div class="row">
         <div class="offset-lg-2 col-lg-8 col-sm-12 pb-3">
+            <p class="mb-2 intro"><span class="font-weight-bold">Vous ne serez pas contacté à la suite de votre signalement.
+                Il ne déclenchera pas à lui seul un contrôle des autorités.</span></p>
 
-            <?php ?>
-            <p class="mb-2 intro"><span class="font-weight-bold">Vous pouvez déposer ici un signalement afin d’informer la DGCCRF</span>
-                <span class="font-italic">(Direction Générale de la Concurrence, Consommation et Répression des Fraudes).</span>
+            <p class="mb-5 intro"><span class="font-weight-bold">Afin de corriger lui-même l’anomalie, le professionnel pourra aussi consulter votre signalement anonymisé.</span>
             </p>
-
-            <p class="mb-2 intro"><span class="font-weight-bold">Vous ne serez pas contacté à la suite de votre signalement.</span>
-                Il ne déclenchera pas à lui seul un contrôle de l’établissement.</p>
-
-            <p class="mb-5 intro"><span class="font-weight-bold">Afin de corriger lui-même l’anomalie, le professionnel pourra aussi consulter votre signalement (sans votre identité).</span>
-            </p>
-            <?php echo form_open_multipart(base_url()); ?>
+            <?php echo form_open_multipart(); ?>
             <div class="form-group">
-                <label for="place-category">Quel type d'établissement souhaitez vous signaler ? </label>
+                <label for="place-category">Quel type d'établissement souhaitez vous signaler ? <span>*</span></label>
                 <select class="form-control form-control-lg chosen-single-select-nosearch is_invalid"
                         id="place-category" name="place-category" data-placeholder="Choisissez ...">
                     <option selected></option>
@@ -37,14 +31,14 @@
                 <?php echo form_error('place-category', '<div class="invalid">', '</div>'); ?>
             </div>
             <div class="form-group">
-                <label for="type">Quel type d'anomalie avez vous constaté ? </label>
+                <label for="type">Quel type d'anomalie avez vous constaté ? <span>*</span></label>
                 <select class="form-control form-control-lg chosen-single-select-nosearch" id="type" name="type"
                         data-placeholder="Choisissez ...">
                     <option selected></option>
                     <?php
                     if ((isset($_POST['place-category'])) && ($_POST['place-category'] != ''))
                         for ($i = 0; isset($anomalies_array[$i]); $i++) {
-                            if (($previous != $anomalies_array[$i][1])&&($anomalies_array[$i][0]==$_POST['place-category'])) {
+                            if (($previous != $anomalies_array[$i][1]) && ($anomalies_array[$i][0] == $_POST['place-category'])) {
                                 echo '<option value="' . $anomalies_array[$i][1] . '"';
                                 if ($anomalies_array[$i][1] == $_POST['type'])
                                     echo ' selected';
@@ -55,15 +49,15 @@
                 </select>
                 <?php echo form_error('type', '<div class="invalid">', '</div>'); ?>
             </div>
-            <div class="form-group mb-4">
-                <label for="anomalies">Précisez</label>
+            <div class="form-group mb-5">
+                <label for="anomalies">Précisez <span>*</span></label>
                 <select class="form-control form-control-lg chosen-single-select" id="anomalies" name="anomalies"
                         data-placeholder="Choisissez une anomalie ...">
                     <option selected></option>
                     <?php
                     if ((isset($_POST['type'])) && ($_POST['type'] != ''))
                         for ($i = 0; isset($anomalies_array[$i]); $i++) {
-                            if (($previous != $anomalies_array[$i][2])&&($anomalies_array[$i][1]==$_POST['type'])&&($anomalies_array[$i][0]==$_POST['place-category'])) {
+                            if (($previous != $anomalies_array[$i][2]) && ($anomalies_array[$i][1] == $_POST['type']) && ($anomalies_array[$i][0] == $_POST['place-category'])) {
                                 echo '<option value="' . $anomalies_array[$i][2] . '"';
                                 if ($anomalies_array[$i][2] == $_POST['anomalies'])
                                     echo ' selected';
@@ -75,40 +69,52 @@
                 <?php echo form_error('anomalies', '<div class="invalid">', '</div>'); ?>
             </div>
             <div class="form-group">
-                <label for="place">Etablissement</label>
-                <input class="form-control" id="place" name="place" placeholder="Nom du commerçant ..."
-                       value="<?php echo set_value('place'); ?>">
+                <label for="place">Nom de l'établissement <span>*</span></label>
+                <input class="form-control" id="place" name="place" value="<?php echo set_value('place'); ?>">
                 <?php echo form_error('place', '<div class="invalid">', '</div>'); ?>
             </div>
             <div class="form-group mb-4">
-                <label for="place-address">Adresse</label>
+                <label for="place-address">Adresse de l'établissement <span>*</span></label>
                 <input class="form-control" name="place-address" id="place-address"
-                       placeholder="numéro, rue et ville ..." value="<?php echo set_value('place-address'); ?>">
+                       value="<?php echo set_value('place-address'); ?>">
                 <?php echo form_error('place-address', '<div class="invalid">', '</div>'); ?>
             </div>
 
             <div class="form-group mb-4">
-                <label for="picture">Ajouter une photo</label>
+                <label for="picture">Vous pouvez ajouter une photo</label>
                 <input type="file" class="form-control-file" name="picture" id="picture"
                        value="<?php echo set_value('picture'); ?>">
             </div>
-            <div class="form-grou mb-4">
-                <label for="description">Précision de votre signalement</label>
+            <div class="form-group mb-5">
+                <label for="description">Vous pouvez préciser votre signalement</label>
                 <textarea class="form-control" name="description" id="description" rows="3" maxlength="200"
                           placeholder="200 caractères maximum"> <?php echo set_value('description'); ?></textarea>
             </div>
-            <p class="font-weight-bold">Votre identité ne sera pas indiquée au professionnel.</p>
+            <p class="font-weight-bold">Votre identité ne sera pas indiquée au professionnel et vous ne serez pas
+                sollicité.</p>
 
-            <div class="form-group">
-                <label for="name">Vos prénom et nom</label>
-                <input class="form-control" name="name" id="name" placeholder="Prénom Nom ..."
-                       value="<?php echo set_value('name'); ?>">
-                <?php echo form_error('name', '<div class="invalid">', '</div>'); ?>
+            <div class="row">
+                <div class="col">
+                    <div class="form-group">
+                        <label for="firstname">Votre prénom</label>
+                        <input type="text" class="form-control" name="firstname" id="firstname"
+                               value="<?php echo set_value('firstname'); ?>">
+                        <?php echo form_error('firstname', '<div class="invalid">', '</div>'); ?>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="form-group">
+                        <label for="lastname">Votre nom</label>
+                        <input type="text" class="form-control" name="lastname" id="lastname"
+                               value="<?php echo set_value('lastname'); ?>">
+                        <?php echo form_error('lastname', '<div class="invalid">', '</div>'); ?>
+                    </div>
+                </div>
+
             </div>
             <div class="form-group mb-5">
                 <label for="email">Votre email </label>
-                <input class="form-control" id="email" name="email" placeholder="votreadresse@mail.fr"
-                       value="<?php echo set_value('email'); ?>">
+                <input class="form-control" id="email" name="email" value="<?php echo set_value('email'); ?>">
                 <?php echo form_error('email', '<div class="invalid">', '</div>'); ?>
             </div>
             <button type="submit" class="btn btn-primary btn-lg btn-block mb-3">Signaler</button>
