d3e2a36dda5d75213224aceee0c2ddd835737f21
Sélim <selim.boudaa@sinusoide.fr>
2018-10-29 14:11:58 +0100

    Insertion report bdd

--- application/controllers/Formulaire.php
+++ application/controllers/Formulaire.php
@@ -53,8 +53,13 @@ class Formulaire extends CI_Controller {
 
 		if ($this->form_validation->run())
 		{
-			$this->load->view('header');
-			$this->load->view('valid');
+			$this->load->model('reports_model');
+			if ($this->reports_model->addReport()){
+				$this->load->view('header');
+				$this->load->view('valid');
+			} else {
+				$data['error'] = true;
+			}
 		}
 		else
 		{
