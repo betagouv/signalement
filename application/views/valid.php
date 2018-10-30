26ae96c3c79bbb31c273d42a62e6b7f321f39a2b
Sélim <selim.boudaa@sinusoide.fr>
2018-10-30 11:17:25 +0100

    ajout message de confirmation

--- application/views/valid.php
+++ application/views/valid.php
@@ -2,6 +2,7 @@
     <div class="row">
         <div class="offset-lg-2 col-lg-8 col-sm-12 pb-3">
             <h2>Merci pour votre signalement</h2>
+            <p class="mt-4">Afin de corriger lui-même l’anomalie, le professionnel pourra aussi consulter votre signalement anonymisé. Il ne déclenchera pas à lui seul un contrôle des autorités.</p>
         </div>
     </div>
 </div>
